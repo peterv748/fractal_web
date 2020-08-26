712f7ad4b19edbf5cafce8b90021217976189dbb
peterv748 <peter.verhoeve@caiway.nl>
2020-08-26 20:34:19 +0200

    commit 26 Augustus 003

--- test_web.py
+++ test_web.py
@@ -83,10 +83,11 @@ def show_home():
 
 @app.route('/fractals')
 def show_fractal_index():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
+    if RedisErrorIsTrue:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
+    else:
+        visits = redis.incr("counter")
+
     return render_template("fractal_index.html", hostname=socket.gethostname(), visits=visits)
 
 #---------------------------------------------------------------------------------------------------------------------
@@ -95,10 +96,11 @@ def show_fractal_index():
 
 @app.route('/<string:imagename>')
 def show_fractal_picture(imagename):
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
+    if RedisErrorIsTrue:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
+    else:
+        visits = redis.incr("counter")
+
 
     image_path = " "
     image_path = imagename + ".png"
@@ -112,10 +114,11 @@ def show_fractal_picture(imagename):
 
 @app.route('/news')
 def show_news():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
+    if RedisErrorIsTrue:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
+    else:
+        visits = redis.incr("counter")
+
     
     temp = {}
     number_of_headlines = 0
@@ -171,10 +174,11 @@ def show_news():
 
 @app.route('/about')
 def show_about():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
+    if RedisErrorIsTrue:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
+    else:
+        visits = redis.incr("counter")
+
 
     return render_template("about.html", hostname=socket.gethostname(), visits=visits)
 
@@ -184,10 +188,11 @@ def show_about():
 
 @app.route('/contact')
 def show_contact():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
+    if RedisErrorIsTrue:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
+    else:
+        visits = redis.incr("counter")
+
 
     return render_template("contact.html", hostname=socket.gethostname(), visits=visits)
 
@@ -204,32 +209,31 @@ def show_web_hook():
     date_time_str = ""
     date_time_now_str=""
     
-    try:
-            visits = redis.incr("counter")
-            if request.method=='POST':
-                 date_time= datetime.now()
-                 date_time_str = f'{date_time:%d-%m-%Y %H:%M:%S}'
-                 message_post= "update: docker stack deploy -c docker-compose.yml fractal has been executed"
-                 redis.set("laststackdeploy", message_post)
-                 redis.set("datetimelaststackdeploy", date_time_str)
+    if not RedisErrorIsTrue:
+        visits = redis.incr("counter")
+        if request.method=='POST':
+           date_time= datetime.now()
+           date_time_str = f'{date_time:%d-%m-%Y %H:%M:%S}'
+           message_post= "update: docker stack deploy -c docker-compose.yml fractal has been executed"
+           redis.set("laststackdeploy", message_post)
+           redis.set("datetimelaststackdeploy", date_time_str)
                 
-            if request.method=='GET':
-                message_get= "no updates sofar"
-                date_time_now = datetime.now() 
-                date_time_now_str = f'{date_time_now:%d-%m-%Y %H:%M:%S}'
-
-            message_post = redis.get("laststackdeploy")
-            date_time_str = redis.get("datetimelaststackdeploy")
-    except RedisError:
-            visits = "<i>cannot connect to Redis, counter disabled</i>"  
-            if request.method=='POST':
-                 date_time= datetime.now()
-                 date_time_str = f'{date_time:%d-%m-%Y %H:%M:%S}'
-                 message_post= "last redeploy: docker stack deploy -c docker-compose.yml fractal has been executed"
-            else:
-                 message_get= "no updates sofar"
-                 date_time_now= datetime.now()    
-                 date_time_now_str = f'{date_time_now:%d-%m-%Y %H:%M:%S}'
+        if request.method=='GET':
+           message_get= "no updates sofar"
+           date_time_now = datetime.now() 
+           date_time_now_str = f'{date_time_now:%d-%m-%Y %H:%M:%S}'
+           message_post = redis.get("laststackdeploy")
+           date_time_str = redis.get("datetimelaststackdeploy")
+    else:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"  
+        if request.method=='POST':
+           date_time= datetime.now()
+           date_time_str = f'{date_time:%d-%m-%Y %H:%M:%S}'
+           message_post= "last redeploy: docker stack deploy -c docker-compose.yml fractal has been executed"
+        else:
+           message_get= "no updates sofar"
+           date_time_now= datetime.now()   
+           date_time_now_str = f'{date_time_now:%d-%m-%Y %H:%M:%S}'
 
     return render_template("web_hook.html", hostname=socket.gethostname(), visits=visits, message_post=message_post, message_get=message_get, date_time=date_time_str, date_time_now=date_time_now_str)
 
@@ -239,10 +243,11 @@ def show_web_hook():
 
 @app.route('/link2')
 def show_link2():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
+    if RedisErrorIsTrue:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
+    else:
+        visits = redis.incr("counter")
+
 
     return render_template("link2.html", hostname=socket.gethostname(), visits=visits)
 
@@ -252,10 +257,11 @@ def show_link2():
 
 @app.route('/link3')
 def show_link3():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
+    if RedisErrorIsTrue:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
+    else:
+        visits = redis.incr("counter")
+
 
     return render_template("link3.html", hostname=socket.gethostname(), visits=visits)
 
